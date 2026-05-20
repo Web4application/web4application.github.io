41f9f23265bd4a3f87ceb6ec1538c8964f745c8e
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-05-20 22:49:19 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,3 +5,15 @@ echo "# azure-cosmosdb-nextjs-starter" >> README.md
   git branch -M main
   git remote add origin https://github.com/QUBUHUB/azure-cosmosdb-nextjs-starter.git
   git push -u origin main
+
+git branch -m QUBUHUBsoftware main
+git fetch origin
+git branch -u origin/main main
+git remote set-head origin -a
+echo "# azure-cosmosdb-nextjs-starter" >> README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git branch -M main
+  git remote add origin https://github.com/QUBUHUB/azure-cosmosdb-nextjs-starter.git
+  git push -u origin main
