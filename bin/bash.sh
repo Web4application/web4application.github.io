c9f487c8a3b713af9b28d6ef8574b059fa7a02a0
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-05-21 07:22:39 +0100

    Update and rename bash.sh to bin/bash.sh

--- bin/bash.sh
+++ bin/bash.sh
@@ -1,3 +1,5 @@
+$ yard graph --protected --full --dependencies
+$ yard server --gems
 echo "# azure-cosmosdb-nextjs-starter" >> README.md
   git init
   git add README.md
@@ -5,6 +7,7 @@ echo "# azure-cosmosdb-nextjs-starter" >> README.md
   git branch -M main
   git remote add origin https://github.com/QUBUHUB/temp.git
   git push -u origin main
+  $ rake yard OPTS='--any --extra --opts'
 git branch -m QUBUHUBsoftware main
 git fetch origin
 git branch -u origin/main main
@@ -17,3 +20,11 @@ git init
   git remote add origin https://github.com/QUBUHUB
   git push -u origin main
   $ gem install yard
+$ yard server  "https://localhost:8808"
+$ yri YARD::Handlers::Base#register
+$ yri File.relative_path
+--query '@return' --query '@param'
+--query '@return && @param`
+--query '@api.text == "public"'
+--query 'object.has_tag?(:api) && object.tag(:api).text == "public"'
+--query 'has_tag?(:api) && tag(:api).text == "public"'
